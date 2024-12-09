78cf5af1d8109f0cb05dc811bebfc30c7cad678b
MiaomiaoShi1004 <simoneshi1004@outlook.com>
2024-12-09 14:17:33 +0100

    test

--- src/components/family-components/CousinB1.tsx
+++ src/components/family-components/CousinB1.tsx
@@ -4,20 +4,46 @@ import { useFamilyContext } from "./hooks/useFamilyContext";
 const CousinB1 = () => {
   const { state } = useFamilyContext();
 
+  // Pass a `disabled` prop to all buttons
+  const isDisabled = state.isB1Visible;
+
   return (
     <div style={{ backgroundColor: "yellow", padding: "20px" }}>
-      {/* <h4>Cousin B1</h4> */}
       <button
-        disabled={!state.isB1Visible}
+        disabled={isDisabled}
         style={{
           padding: "10px",
-          backgroundColor: state.isB1Visible ? "black" : "gray",
+          backgroundColor: isDisabled ? "gray" : "black",
           color: "white",
-          cursor: state.isB1Visible ? "pointer" : "not-allowed",
-          border: "none",
+          cursor: isDisabled ? "not-allowed" : "pointer",
+          margin: "5px",
         }}
       >
-        Cousin B1
+        Sing
+      </button>
+      <button
+        disabled={isDisabled}
+        style={{
+          padding: "10px",
+          backgroundColor: isDisabled ? "gray" : "black",
+          color: "white",
+          cursor: isDisabled ? "not-allowed" : "pointer",
+          margin: "5px",
+        }}
+      >
+        Dance
+      </button>
+      <button
+        disabled={isDisabled}
+        style={{
+          padding: "10px",
+          backgroundColor: isDisabled ? "gray" : "black",
+          color: "white",
+          cursor: isDisabled ? "not-allowed" : "pointer",
+          margin: "5px",
+        }}
+      >
+        Sport
       </button>
     </div>
   );
