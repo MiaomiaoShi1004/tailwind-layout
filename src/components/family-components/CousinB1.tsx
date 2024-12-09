6725910e8ba2ca75c83992207ebb22c17034b81c
MiaomiaoShi1004 <simoneshi1004@outlook.com>
2024-12-09 13:56:31 +0100

    change childB1 to button

--- src/components/family-components/CousinB1.tsx
+++ src/components/family-components/CousinB1.tsx
@@ -4,11 +4,21 @@ import { useFamilyContext } from "./hooks/useFamilyContext";
 const CousinB1 = () => {
   const { state } = useFamilyContext();
 
-  if (!state.isB1Visible) return null;
-
   return (
     <div style={{ backgroundColor: "yellow", padding: "20px" }}>
-      <h4>Cousin B1</h4>
+      {/* <h4>Cousin B1</h4> */}
+      <button
+        disabled={!state.isB1Visible}
+        style={{
+          padding: "10px",
+          backgroundColor: state.isB1Visible ? "black" : "gray",
+          color: "white",
+          cursor: state.isB1Visible ? "pointer" : "not-allowed",
+          border: "none",
+        }}
+      >
+        Cousin B1
+      </button>
     </div>
   );
 };
